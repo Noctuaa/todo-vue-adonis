4e28268654201ac2d4721c47855d58ea20cc6130
Noctua <titgeogeo@gmail.com>
2025-07-19 18:30:04 +0200

    fix: adjust email column length to 191 characters in users table migration

--- backend/database/migrations/1752595191029_create_users_table.ts
+++ backend/database/migrations/1752595191029_create_users_table.ts
@@ -5,11 +5,11 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
+      table.engine('InnoDB')
       table.increments('id').notNullable()
       table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
+      table.string('email', 191).notNullable().unique()
       table.string('password').notNullable()
-
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
