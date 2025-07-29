bdcc47a1aeae8c86999c28c7552831da8dc721b5
Noctua <titgeogeo@gmail.com>
2025-07-29 18:47:24 +0200

    refactor: replace HomeView with TodosView and update route names

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/TodosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'todos',
       redirect: () => {
         return '/login'
       }
